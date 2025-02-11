c7e1228af4d571d4d78dbdeb01199e9c3df83f30
afrozetk <afroze.tariq@gmail.com>
2025-02-10 23:04:46 -0600

    model to create a listing

---
+++ listings/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Listing(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title=models.CharField(max_length=64, null=True, blank=True)
+    isbn=models.CharField(max_length=64)
+    author=models.CharField(max_length=64, null=True, blank=True)
+    description=models.TextField(null=True, blank=True)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    # image=models.ImageField(upload_to="lisitng_images/", blank=True, null=True)
+    CONDITION_CHOICES = [
+    ('New', 'New'),
+    ('Like New', 'Like New'),
+    ('Good', 'Good'),
+    ('Acceptable', 'Acceptable'),
+]
+    condition = models.CharField(max_length=20, choices=CONDITION_CHOICES)
+    LOCATION_CHOICES = [
+    ('Global', 'Global'),
+    ('Local', 'Local'),
+    ]
+    location=models.CharField(max_length=64, choices=LOCATION_CHOICES)
+    coursecode=models.CharField(max_length=64, null=True, blank=True)
+
+    def __str__(self):  
+        return f"{self.title} by {self.author} for {self.price}"
