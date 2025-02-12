77662eb8b09797895581913709a9d76d3daf89d4
afrozetk <afroze.tariq@gmail.com>
2025-02-11 19:29:37 -0600

    modified listing url

--- textbookxchange/urls.py
+++ textbookxchange/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
-     path("listings/", include("listings.urls")),
+    path("listings/", include("listings.urls", namespace='listings')),
 ]
