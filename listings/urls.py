e61e87baf34f510f98daf1c49a282996c487abe1
afrozetk <afroze.tariq@gmail.com>
2025-02-11 19:28:14 -0600

    added new url to make test guiseppe code work

--- listings/urls.py
+++ listings/urls.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
-
+from .views import EbayPriceScraperView
+app_name = 'listings'
 urlpatterns = [
     path("create/", views.create_listing, name="create_listing"),
+    path("fetch_ebay_price/", EbayPriceScraperView.as_view(), name="fetch_ebay_price"),
 ]
