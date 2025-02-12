845f55b74ea0ca2074e1db409f941dbc168624fe
afrozetk <afroze.tariq@gmail.com>
2025-02-12 09:21:31 -0600

    added lisitings page url

--- listings/urls.py
+++ listings/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 from . import views
 from .views import EbayPriceScraperView
 app_name = 'listings'
 urlpatterns = [
     path("create/", views.create_listing, name="create_listing"),
     path("fetch_ebay_price/", EbayPriceScraperView.as_view(), name="fetch_ebay_price"),
+    path("all/", views.listing_page, name="listing_page"), 
 ]
