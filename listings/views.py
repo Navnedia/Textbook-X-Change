71fa9056e755c092f6486ced098c819795003e20
Navnedia <87249556+Navnedia@users.noreply.github.com>
2025-02-13 00:03:32 -0600

    Added prelisting autofill page
    The user is prompted to enter their ISBN number and they are then directed to the create listing page details prefilled (when avaliable).

--- listings/views.py
+++ listings/views.py
@@ -5,12 +5,27 @@ from .forms import ListingForm
 
 # Create your views here.
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .forms import ListingForm
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from .forms import ListingForm, PrelistForm
 from .models import Listing
+from .services.autofill import PrelistSuggestionsProvider
+# Create your views here:
 
-def create_listing(request):
-    if request.method == "POST":
+
+def prelist(request: HttpRequest) -> HttpResponse:
+    if request.POST:
+        form = PrelistForm(request.POST)
+        if form.is_valid():
+            provider = PrelistSuggestionsProvider()
+            listing = provider.process_data(isbn=form.cleaned_data["isbn"])
+            
+            return create_listing(request, autofill_data=listing)
+
+    return render(request, "prelist.html", {"form": PrelistForm()})
+
+
+def create_listing(request: HttpRequest, autofill_data: Listing | None = None) -> HttpResponse:
+    if not autofill_data and request.method == "POST":
         form = ListingForm(request.POST, request.FILES)    
         if form.is_valid():
             form.save()
@@ -19,8 +34,9 @@ def create_listing(request):
             print(form.errors)
 
     else:
-        form = ListingForm()
+        form = ListingForm(instance=autofill_data)
     return render(request, "create_listing.html", {"form": form})
+
 
 def listing_page(request):
     listings = Listing.objects.all().order_by("-id")  
