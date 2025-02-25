1b4e643eb471d741a863367b3c5e87c5cf5a256b
afrozetk <afroze.tariq@gmail.com>
2025-02-24 23:19:26 -0600

    added new URLS for edit, dashboard and delete

--- listings/urls.py
+++ listings/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path("prelist/", views.prelist, name="prelist"),
     path("create/", views.create_listing, name="create_listing"),
     path("fetch_ebay_price/", EbayPriceScraperView.as_view(), name="fetch_ebay_price"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+     path("listing/edit/<int:listing_id>/", views.edit_listing, name="edit_listing"),
+    path("listing/delete/<int:listing_id>/", views.delete_listing, name="delete_listing")
 ]
