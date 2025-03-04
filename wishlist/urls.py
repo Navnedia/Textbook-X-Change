f1c90dbfd57ea1b4a5e002bc4cce33586f2ee929
kian544 <147429923+kian544@users.noreply.github.com>
2025-03-03 20:28:02 -0600

    Requests Page Edits
    Created "My Listings", beginning "edit_request" and delete function. Added button to request submission. Message now only appears after submission is complete.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,4 +6,7 @@ app_name = 'wishlist'
 urlpatterns = [
     path("createwish", views.create_wish, name="request"),
     path("requestbook", views.request_book, name="requestBook"),
+    path("myrequests", views.my_requests, name="myrequests"),
+    path("edit_request/<int:request_id>", views.edit_request, name="edit_request"),  # Edit request URL
+    path("delete_request/<int:request_id>", views.delete_request, name="delete_request"),  # Delete request URL
 ]
