6479211e896212b3e36409fefb06661f589edae2
Navnedia <87249556+Navnedia@users.noreply.github.com>
2025-03-04 06:34:41 -0600

    Model context comments

--- core/models.py
+++ core/models.py
@@ -7,8 +7,11 @@ from django.db.models.functions import Lower
 
 class School(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+    # This might be relevant later if we wanted to autoselect a school based on account email domain.
     website = models.URLField(null=True, blank=True)
 
+    # These location details would be relevant later if we choose to do more fine grain radius location searching, etc.
     long = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
     lat = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
 
