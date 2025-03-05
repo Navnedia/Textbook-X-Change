a4079f33e79d24b584fadcc018f6bb62ced40bb5
Navnedia <87249556+Navnedia@users.noreply.github.com>
2025-03-05 09:52:14 -0600

    Reorder model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class WishList(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=13)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
