21ac38a478f0ddf7bbd6ca639f6dcbc0c4d7077f
acanady123 <canady.a@outlook.com>
2025-03-10 19:42:30 -0500

    Added the ability to shuffle through images on textbook details page and created the user_profile.html. Still working on the functionality of linking the page to the logged in user

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", TemplateView.as_view(template_name="core/index.html"), name="index"),
     path("about/", TemplateView.as_view(template_name="core/about.html"), name="about"),  # About Page
+     path("profile/", TemplateView.as_view(template_name="core/user_profile.html"), name="user_profile"),  # User Profile Page
 ]
 
 
