858b983c4d46dee1d23f8007ea2951537774baf0
Navnedia <87249556+Navnedia@users.noreply.github.com>
2025-03-12 16:49:33 -0500

    Moved buyer order shipped badge to cart

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,15 +1,19 @@
 from cart.models import Order
-from listings.models import Listing
 from django.db.models import Q
 from django.http import HttpRequest
 
 def notifications(request: HttpRequest):
     if request.user.is_authenticated:
         # If the user is a seller with an item that needs to be shipped, or a buyer when the item has shipped.
-        shipping_status = Order.objects.filter(
-            Q(listing__seller=request.user, has_shipped=False) |
+        seller_ship_order = Order.objects.filter(
+            Q(listing__seller=request.user, has_shipped=False)
+        ).exists()
+        buyer_order_shipped = Order.objects.filter(
             Q(buyer=request.user, has_shipped=True)
         ).exists()
-        return { "dashboard_notification": shipping_status }
+        return { 
+            "dashboard_notification": seller_ship_order,
+            "cart_notification": buyer_order_shipped
+        }
     
     return {}
