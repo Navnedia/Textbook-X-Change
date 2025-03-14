55cd3a0a98e1bbb66d6e0c76ce9133efb5a462f6
Navnedia <87249556+Navnedia@users.noreply.github.com>
2025-03-14 05:54:44 -0500

    Admin Panel Improvements
    I configured filtering parameters and added inline support for adding images and orders when you create a listing

--- cart/admin.py
+++ cart/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Order
 
 # Register your models here.
-admin.site.register(Order)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'buyer', 'shipping_address', 'has_shipped', 'created_at')
+    search_fields = ('listing__title', 'buyer__username', 'shipping_address')
+    list_filter = ('has_shipped', 'created_at')
--- cart/models.py
+++ cart/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 # Create your models here.
 class Order(models.Model):
     buyer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True, blank=True)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True, blank=True, db_index=True)
     shipping_address = models.CharField(max_length=255, blank=True, null=True)
     has_shipped = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
