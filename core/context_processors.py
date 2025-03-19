bf96f785e01d5026bf9442be3269e6e80dfb8701
Navnedia <87249556+Navnedia@users.noreply.github.com>
2025-03-19 15:14:57 -0500

    Added notification badge back to new navbar design

--- core/context_processors.py
+++ core/context_processors.py
@@ -7,12 +7,12 @@ def notifications(request: HttpRequest):
         # If the user is a seller with an item that needs to be shipped, or a buyer when the item has shipped.
         seller_ship_order = Order.objects.filter(
             Q(listing__seller=request.user, has_shipped=False)
-        ).exists()
+        ).count()
         buyer_order_shipped = Order.objects.filter(
             Q(buyer=request.user, has_shipped=True)
         ).exists()
         return { 
-            "dashboard_notification": seller_ship_order,
+            "dashboard_notifications": seller_ship_order,
             "cart_notification": buyer_order_shipped
         }
     
